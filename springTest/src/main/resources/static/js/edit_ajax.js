e2d43d04317bd77b44687b29c7922a0b8ac086e4
Pasha <qordon23@gmail.com>
2022-07-20 01:18:00 +0400

    vvel nekotorie izmenenie v ajax i dobavil method put

--- springTest/src/main/resources/static/js/edit_ajax.js
+++ springTest/src/main/resources/static/js/edit_ajax.js
@@ -1,19 +1,21 @@
-$('#save').click(function () {
-    let name = $('#name').val()
-    let surname = $('#surname').val()
-    let age = $('#age').val()
+$(document).ready(function (){
+    $('#edit').click(function () {
+        let name = $('#name').val()
+        let surname = $('#surname').val()
+        let age = $('#age').val()
 
-    let data = {}
+        let data = {}
 
-    data.name = name
-    data.surname = surname
-    data.age = age
+        data.name = name
+        data.surname = surname
+        data.age = age
 
-    $.ajax({
-        url: '/main/customers/edit/{id}',
-        type: 'POST',
-        data: JSON.stringify(data),
-        dataType: 'json',
-        contentType: "application/json"
-    });
+        $.ajax({
+            url: '/main/customers/edit/{id}',
+            type: 'POST',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: "application/json"
+        });
+    })
 })
