ad1a64843668efd9daeada2c4d84673eab354069
Pasha <qordon23@gmail.com>
2022-08-02 01:34:33 +0400

    fixed edit_ajax.js and edit_customer.html

--- springTest/src/main/resources/static/js/edit_ajax.js
+++ springTest/src/main/resources/static/js/edit_ajax.js
@@ -3,22 +3,22 @@ $(document).ready(function (){
         let name = $('#name').val()
         let surname = $('#surname').val()
         let age = $('#age').val()
-        // let id = $('#id').val()
+        let id = $('#id').val()
 
         let data = {}
         data.name = name
         data.surname = surname
         data.age = age
 
-        var formData = new FormData();
-        formData.append("name",name);
-        formData.append("surname",surname);
-        formData.append("age",age);
+        // var formData = new FormData();
+        // formData.append("name",name);
+        // formData.append("surname",surname);
+        // formData.append("age",age);
 
         $.ajax({
-            url: window.location + '/main/customers/put/edit/{id}',
-            type: 'PUT',
-            data: JSON.stringify(formData),
+            url: window.location + '/main/customers/edit/' + id,
+            type: 'POST',
+            data: JSON.stringify(data),
             dataType: 'json',
             contentType: "application/json"
         });
